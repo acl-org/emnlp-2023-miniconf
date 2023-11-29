34fb9106cb59fa914352a5905bdf10c32f7b70ec
Yihang Lin <913875152@qq.com>
2023-11-29 20:08:00 +0800

    Create sh file to process data
    We created a sh file include 9 commands to process data.Need to run the code "sh pipeline.sh" only.

---
+++ pipeline.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python data/emnlp_2023/data/generate_booklet_data_json.py &&
+python data/emnlp_2023/data/write_event_to_booklet.py &&
+python data/emnlp_2023/data/generate_workshops_yaml.py &&
+python emnlp_miniconf/import_emnlp2023.py &&
+python data/emnlp_2023/data/generate_break_display.py &&
+python data/emnlp_2023/data/write_break_display_to_conference.py &&
+python data/emnlp_2023/data/generate_coffee_break_json.py &&
+python data/emnlp_2023/data/write_break_to_conference.py &&
+python data/emnlp_2023/data/write_social_event_display_to_confenence.py
