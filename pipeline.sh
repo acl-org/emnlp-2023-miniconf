d51b571430fe7c7de19d5cb7c18199a5bfeafc69
Yihang Lin <913875152@qq.com>
2023-11-30 16:40:53 +0800

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -4,6 +4,8 @@ python data/emnlp_2023/data/generate_booklet_data_json.py &&
 python data/emnlp_2023/data/write_event_to_booklet.py &&
 python data/emnlp_2023/data/generate_workshops_yaml.py &&
 python emnlp_miniconf/import_emnlp2023.py &&
+python data/emnlp_2023/data/generate_BoF_display.py &&
+python data/emnlp_2023/data/write_BoF_display_to_conference.py &&
 python data/emnlp_2023/data/generate_break_display.py &&
 python data/emnlp_2023/data/write_break_display_to_conference.py &&
 python data/emnlp_2023/data/generate_coffee_break_json.py &&
